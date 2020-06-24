260519bd24e031c4d8027502abb8e6215681d04e
Federico Agustin Barón <federicobaron@fb.com>
2020-06-23 20:51:01 -0400

    Added UI and RecyclerView shows items

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button addButton;
+    EditText editText;
+    RecyclerView viewItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addButton);
+        editText = findViewById(R.id.editItem);
+        viewItems = findViewById(R.id.viewItems);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the Gym");
+        items.add("Buy cheese");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        viewItems.setAdapter(itemsAdapter);
+        viewItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }
