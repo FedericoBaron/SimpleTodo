046b00d1fc1b979ddc5932432a35417bd97aef99
Federico Agustin Barón <federicobaron@fb.com>
2020-06-23 23:41:23 -0400

    Edit function working

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -2,7 +2,9 @@ package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,5 +22,27 @@ public class EditActivity extends AppCompatActivity {
         saveButton = findViewById(R.id.saveButton);
 
         getSupportActionBar().setTitle("Edit item");
+
+        // Set text to be the item that's being edited
+        editItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // When the user is done editing, they click the save button
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent which will contain the results
+                Intent intent = new Intent();
+
+                // Pass the data (results of editing)
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, editItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                // Set the result of the intent
+                setResult(RESULT_OK, intent);
+
+                // Finish the activity, close the screen and go back
+                finish();
+            }
+        });
     }
 }
